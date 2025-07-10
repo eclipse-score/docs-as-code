8fc03e2239ec7fde54efd7944a9dd0200a25732b
Maximilian Pollak <maximilian.pollak@expleogroup.com>
2025-07-10 12:30:32 +0200

    WIP

--- src/tests/test_consumer.py
+++ src/tests/test_consumer.py
@@ -3,23 +3,42 @@ from collections.abc import Callable
 from pathlib import Path
 
 import subprocess
+from sys import stderr
 import pytest
 import os
 import re
+import logging
+
 from pytest import TempPathFactory
 from typing import Optional
 from sphinx.testing.util import SphinxTestApp
 from sphinx_needs.data import SphinxNeedsData
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+
+# Do not need the sphinx logger => Normal python logger instead.
+LOGGER = logging.getLogger(__name__)
+
 
 @dataclass
-class ConsumerRepo():
+class ConsumerRepo:
     name: str
     git_url: str
-    commands: list[str] 
+    commands: list[str]
     test_commands: list[str]
     LocalOverrideResult: bool
     GitOverrideResult: bool
+
+
+@dataclass
+class BuildResult:
+    returncode: int
+    stdout: str
+    stderr: str
+    warnings: dict[str, list[str]] = field(default_factory=dict)
+    errors: list[str] = field(default_factory=list)
+    infos: list[str] = field(default_factory=list)
+    metamodel_warnings: list[str] = field(default_factory=list)
+    other_warnings: list[str] = field(default_factory=list)
 
 
 REPOS_TO_TEST: list[ConsumerRepo] = [
@@ -31,34 +50,34 @@ REPOS_TO_TEST: list[ConsumerRepo] = [
     #         GitOverrideResult=False,
     #         test_commands= []
     # ),
-    # ConsumerRepo(
-    #         name="score",
-    #         git_url= "git@github.com:eclipse-score/score.git",
-    #         commands=[
-    #             "bazel run //docs:incremental_latest",
-    #             "bazel run //docs:incremental_release",
-    #             "bazel run //docs:ide_support",
-    #             "bazel build //docs:docs_release",
-    #             "bazel build //docs:docs_latest",
-    #         ],
-    #         LocalOverrideResult=False,
-    #         GitOverrideResult=False,
-    #         test_commands= []
-    # ),
     ConsumerRepo(
-            name="module_template",
-            git_url= "git@github.com:eclipse-score/module_template.git",
-            commands=[
-                "bazel run //docs:ide_support",
-                # "bazel run //docs:incremental",
-                # "bazel build //docs:docs",
-            ],
-            LocalOverrideResult=False,
-            GitOverrideResult=False,
-            test_commands= [
-            "bazel test //tests/...",
-        ]
-    )
+        name="score",
+        git_url="git@github.com:eclipse-score/score.git",
+        commands=[
+            #"bazel run //docs:incremental_latest",
+            "bazel run //docs:incremental_release",
+            # "bazel run //docs:ide_support",
+            # "bazel build //docs:docs_release",
+            # "bazel build //docs:docs_latest",
+        ],
+        LocalOverrideResult=False,
+        GitOverrideResult=False,
+        test_commands=[],
+    ),
+    # ConsumerRepo(
+    #     name="module_template",
+    #     git_url="git@github.com:eclipse-score/module_template.git",
+    #     commands=[
+    #         "bazel run //docs:ide_support",
+    #         "bazel run //docs:incremental",
+    #         "bazel build //docs:docs",
+    #     ],
+    #     LocalOverrideResult=False,
+    #     GitOverrideResult=False,
+    #     test_commands=[
+    #         "bazel test //tests/...",
+    #     ],
+    # )
 ]
 
 
@@ -67,7 +86,7 @@ def sphinx_base_dir(tmp_path_factory: TempPathFactory) -> Path:
     return tmp_path_factory.mktemp("testing_dir")
 
 
-def get_current_git_commit(curr_path):
+def get_current_git_commit(curr_path: Path):
     """
     Get the current git commit hash (HEAD).
     """
@@ -81,52 +100,124 @@ def get_current_git_commit(curr_path):
     return result.stdout.strip()
 
 
+def get_remote_main_commit(curr_path: Path) -> str:
+    """
+    Get the commit hash of the remote main branch, to find possible metamodel changes
+    """
+    _ = subprocess.run(
+        ["git", "fetch", "origin", "main"],
+        capture_output=True,
+        text=True,
+        check=True,
+        cwd=curr_path,
+    )
+
+    result = subprocess.run(
+        ["git", "rev-parse", "origin/main"],
+        capture_output=True,
+        text=True,
+        check=True,
+        cwd=curr_path,
+    )
+    LOGGER.debug(f"Grabbed remote origin/main with commit: {result.stdout.strip()}")
+    return result.stdout.strip()
+
+
+def check_for_metamodel_change(curr_path: Path, remote_base_commit: str):
+    """
+    Check if there are any changes in the metamodel.
+    """
+    try:
+        # Get list of changed files
+        result = subprocess.run(
+            ["git", "diff", "--name-only", remote_base_commit, "HEAD"],
+            capture_output=True,
+            text=True,
+            check=True,
+            cwd=curr_path,
+        )
+
+        changed_files = result.stdout.strip().split("\n")
+
+        # Check if any changed files are in metamodel paths
+        for file_path in changed_files:
+            if "metamodel.yaml" in file_path:
+                LOGGER.debug(
+                    "Metamodel.yaml has changed. Ignoring 'score_metamodel' errors."
+                )
+                return True
+
+        LOGGER.debug("Metamodel.yaml has NOT changed. Parsing all errors")
+        return False
+
+    except Exception as e:
+        # Wondering if this is a good idea.
+        # Or better to fail here?
+        LOGGER.debug(f"Failed git diff. Error: {e}")
+        return False
 
 
 def replace_bazel_dep_with_local_override(module_content):
-    """
-    """
-    
+    """ """
+
     # Pattern to match the bazel_dep line
-    pattern = rf'bazel_dep\(name = "score_docs_as_code", version = "0.4.1"\)'
-    
+    pattern = rf'bazel_dep\(name = "score_docs_as_code", version = "0.4.2"\)'
+
     # Replacement with local_path_override
-    replacement = f'''bazel_dep(name = "score_docs_as_code", version = "0.4.1")
+    replacement = f"""bazel_dep(name = "score_docs_as_code", version = "0.4.2")
 local_path_override(
     module_name = "score_docs_as_code",
     path = "../docs_as_code"
-)'''
-    
+)"""
+
     # Replace the pattern
     modified_content = re.sub(pattern, replacement, module_content)
-    
+
     return modified_content
 
+
 def replace_bazel_dep_with_git_override(module_content, git_hash):
-    """
-    """
+    """ """
     import re
-    
+
     # Pattern to match the bazel_dep line
-    pattern = rf'bazel_dep\(name = "score_docs_as_code", version = "0.4.1"\)'
-    
+    pattern = rf'bazel_dep\(name = "score_docs_as_code", version = "0.4.2"\)'
+
     # Replacement with local_path_override
-    replacement = f'''bazel_dep(name = "score_docs_as_code", version = "0.4.1")
+    replacement = f'''bazel_dep(name = "score_docs_as_code", version = "0.4.2")
 git_override(
     module_name = "score_docs_as_code",
     repsoitory = 
     commit = "{git_hash}"
 )'''
-    
+
     # Replace the pattern
     modified_content = re.sub(pattern, replacement, module_content)
-    
+
     return modified_content
 
+
+def parse_bazel_output(BR: BuildResult) -> BuildResult:
+    print(f"STDOUT: {BR.stdout}")
+    print("=========================")
+    print("=========================")
+    print("=========================")
+    print(f"stderr: {BR.stderr}")
+    print("=========================")
+    print("=========================")
+    print("=========================")
+    return BR
+
+
 def test_and_clone_repos(sphinx_base_dir):
-    # Clone 
+    # Clone
+    # This might not be the best idea
     os.chdir(sphinx_base_dir)
-    current_hash = get_current_git_commit(Path(__file__).parent)
+    curr_path = Path(__file__).parent
+    current_hash = get_current_git_commit(curr_path)
+    remote_main_commit = get_remote_main_commit(curr_path)
+    metamodel_changed = check_for_metamodel_change(curr_path, remote_main_commit)
+
     for repo in REPOS_TO_TEST:
         # CLoning the repo
         subprocess.run(["git", "clone", repo.git_url], check=True, capture_output=True)
@@ -134,29 +225,40 @@ def test_and_clone_repos(sphinx_base_dir):
         with open("MODULE.bazel", "r") as f:
             module_orig = f.read()
         module_local_override = replace_bazel_dep_with_local_override(module_orig)
-        module_git_override = replace_bazel_dep_with_git_override(module_orig, current_hash)
+        module_git_override = replace_bazel_dep_with_git_override(
+            module_orig, current_hash
+        )
         with open("MODULE.bazel", "w") as f:
             f.write(module_local_override)
         # TEST all commands
         for cmd in repo.commands:
             print("BUILDING WITH LOCAL OVERRIDE")
-            out = subprocess.run(cmd.split(), capture_output=True, check=True, text=True)
-            #print(out)
-            #assert out.returncode == 0
-            assert "Build completed successfully" in str(out.stderr) 
+            out = subprocess.run(
+                cmd.split(), capture_output=True, check=True, text=True
+            )
+            BR = BuildResult(
+                returncode=out.returncode, stdout=out.stdout, stderr=out.stderr
+            )
+            parse_bazel_output(BR)
+
+            # print(out)
+            # assert out.returncode == 0
+            assert "Build completed successfully" in str(out.stderr)
         for test_cmd in repo.test_commands:
             print("TESTING WITH LOCAL OVERRIDE")
-            out = subprocess.run(test_cmd.split(), capture_output=True, check=True, text=True)
+            out = subprocess.run(
+                test_cmd.split(), capture_output=True, check=True, text=True
+            )
             assert out.returncode == 0
         with open("MODULE.bazel", "w") as f:
             f.write(module_git_override)
         for cmd in repo.commands:
             print("TESTING WITH GIt OVERRIDE")
-            out = subprocess.run(cmd.split(), capture_output=True, check=True, text=True)
-            #print(out)
-            #assert out.returncode == 0
+            out = subprocess.run(
+                cmd.split(), capture_output=True, check=True, text=True
+            )
+            # print(out)
+            # assert out.returncode == 0
             assert "Build completed successfully" in str(out.stderr)
         os.chdir(Path.cwd().parent)
         # Now need to adapt the MODULE.bazel file.
-   
-
