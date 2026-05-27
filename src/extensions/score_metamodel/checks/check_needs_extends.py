31d5653c32bc1ad2c7ff15c67db2e77fffa77d28
MaximilianSoerenPollak <maximilian.pollak@qorix.com>
2026-05-27 16:53:20 +0200

    Feat: Add needextend checks and tests

--- src/extensions/score_metamodel/checks/check_needs_extends.py
+++ src/extensions/score_metamodel/checks/check_needs_extends.py
@@ -13,24 +13,18 @@
 from __future__ import annotations
 
 import sphinx_needs.directives.need
-from docutils import nodes
 from sphinx_needs.config import NeedsSphinxConfig
 from sphinx_needs.data import ExtendType, NeedsExtendType, NeedsMutable
 from sphinx_needs.exceptions import NeedsInvalidFilter
 from sphinx_needs.filter_common import filter_needs_mutable
 from sphinx_needs.logging import get_logger, log_warning
-from sphinx_needs.need_item import NeedModification
+from sphinx_needs.directives.needextend import extend_needs_data as original_function
 from sphinx_needs.needs_schema import (
     FieldFunctionArray,
     FieldLiteralValue,
     LinksFunctionArray,
     LinksLiteralValue,
 )
-
-
-class Needextend(nodes.General, nodes.Element):
-    pass
-
 
 logger = get_logger(__name__)
 
@@ -39,7 +33,7 @@ def score_extend_needs_data_func(
     all_needs: NeedsMutable,
     extends: dict[str, NeedsExtendType],
     needs_config: NeedsSphinxConfig,
-) -> None:
+):
     """Use data gathered from needextend directives to modify fields of existing needs."""
 
     # Sort by (docname, lineno) to ensure deterministic ordering,
@@ -47,10 +41,13 @@ def score_extend_needs_data_func(
     sorted_extends = sorted(extends.values(), key=lambda x: (x["docname"], x["lineno"]))
 
     current_needextend: NeedsExtendType
-    raise RuntimeError("TESING replacement func")
     for current_needextend in sorted_extends:
         need_filter = current_needextend["filter"]
         location = (current_needextend["docname"], current_needextend["lineno"])
+        if "c.this_doc()" not in need_filter:
+            error_msg = "Potentially altering needs outside of the document is not allowed. Please add 'c.this_doc()' to the needextend to limit it to only needs in the same document"
+            log_warning(logger, error_msg, "needextend", location=location)
+
         if current_needextend["filter_is_id"]:
             try:
                 found_needs = [all_needs[need_filter]]
@@ -78,14 +75,15 @@ def score_extend_needs_data_func(
                 )
                 continue
         for found_need in found_needs:
+            if found_need["is_external"]:
+                log_warning(
+                    logger,
+                    "Error. It is not allowed to modify external needs via needextend",
+                    "needextend",
+                    location,
+                )
             # Work in the stored needs, not on the search result
             need = all_needs[found_need["id"]]
-            need.add_modification(
-                NeedModification(
-                    docname=current_needextend["docname"],
-                    lineno=current_needextend["lineno"],
-                )
-            )
 
             location = (
                 current_needextend["docname"],
@@ -95,135 +93,39 @@ def score_extend_needs_data_func(
             for option_name, etype, link_value in current_needextend[
                 "list_modifications"
             ]:
-                # append link = ok?!
-                # replace / remove link = nope
-                # set option = ok
-                # replace / remove option = nope
                 match (etype, link_value):
-                    case (ExtendType.APPEND, LinksLiteralValue()):
-                        if (df := need._dynamic_fields.get(option_name)) is not None:
-                            need._dynamic_fields[option_name] = LinksFunctionArray(
-                                (*df.value, *link_value.value)
-                            )
-                            need[option_name] = []
-                        else:
-                            existing = need.get_links(option_name, as_str=False)
-                            need[option_name] = [
-                                *existing,
-                                *(  # keep unique
-                                    v for v in link_value.value if v not in existing
-                                ),
-                            ]
-                    case (ExtendType.APPEND, LinksFunctionArray()):
-                        if (df := need._dynamic_fields.get(option_name)) is not None:
-                            need._dynamic_fields[option_name] = LinksFunctionArray(
-                                (  # keep unique
-                                    *df.value,
-                                    *(v for v in link_value.value if v not in df.value),
-                                )
-                            )
-                            need[option_name] = []
-                        else:
-                            existing = need.get_links(option_name, as_str=False)
-                            need._dynamic_fields[option_name] = LinksFunctionArray(
-                                (
-                                    *existing,
-                                    *(  # keep unique
-                                        v for v in link_value.value if v not in existing
-                                    ),
-                                )
-                            )
-                            need[option_name] = []
-                    case (ExtendType.REPLACE | ExtendType.DELETE, LinksLiteralValue()):
+                    case (
+                        ExtendType.REPLACE | ExtendType.DELETE,
+                        LinksLiteralValue() | LinksFunctionArray(),
+                    ):
+                        # Replacing / Deleting links is not allowed
                         error_msg = (
                             "Replace or Delete action is not allowed via needextends."
                         )
                         log_warning(logger, error_msg, "needextend", location=location)
-                        raise RuntimeError(f"{location}: {error_msg}")
-                    case (ExtendType.REPLACE | ExtendType.DELETE, LinksFunctionArray()):
-                        error_msg = (
-                            "Replace or Delete action is not allowed via needextends."
-                        )
-                        log_warning(logger, error_msg, "needextend", location=location)
-                        raise RuntimeError(f"{location}: {error_msg}")
-                    case other_link:
-                        raise RuntimeError(
-                            f"Unhandled case {other_link} for {option_name!r}"
-                        )
 
             for option_name, etype, field_value in current_needextend["modifications"]:
+                if etype == ExtendType.DELETE:
+                    error_msg = "Delete action is not allowed via needextends."
+                    log_warning(logger, error_msg, "needextend", location=location)
                 match (etype, field_value):
                     case (ExtendType.APPEND, FieldLiteralValue()):
-                        if (df := need._dynamic_fields.get(option_name)) is not None:
-                            need._dynamic_fields[option_name] = (
-                                FieldFunctionArray((*df.value, *field_value.value))
-                                if isinstance(field_value.value, list)
-                                else FieldFunctionArray((*df.value, field_value.value))
+                        if isinstance(field_value.value, str):
+                            error_msg = "Append action is not allowed via needextends on 'string type options'."
+                            log_warning(
+                                logger, error_msg, "needextend", location=location
                             )
-                        else:
-                            if isinstance(field_value.value, list):
-                                need[option_name] = [
-                                    *need[option_name],
-                                    *field_value.value,
-                                ]
-                            elif isinstance(field_value.value, str):
-                                need[option_name] = (
-                                    need[option_name] + " " + field_value.value
-                                    if need[option_name]
-                                    else field_value.value
-                                )
-                            else:
-                                raise RuntimeError(
-                                    f"Cannot append non-string/array value {field_value.value!r} to field '{option_name}'"
-                                )
-                    case (ExtendType.APPEND, FieldFunctionArray()):
-                        if (df := need._dynamic_fields.get(option_name)) is not None:
-                            need._dynamic_fields[option_name] = FieldFunctionArray(
-                                (*df.value, *field_value.value)
+
+                    case (
+                        ExtendType.REPLACE,
+                        None | FieldLiteralValue() | FieldFunctionArray(),
+                    ):
+                        if need[option_name]:
+                            error_msg = f"Error when extending need: {need['id']}. Replacing of options that are already set is not allowed via needextends."
+                            log_warning(
+                                logger, error_msg, "needextend", location=location
                             )
-                        else:
-                            if isinstance(need[option_name], list):
-                                need._dynamic_fields[option_name] = FieldFunctionArray(
-                                    (*need[option_name], *field_value.value)
-                                )
-                            elif isinstance(need[option_name], str):
-                                need._dynamic_fields[option_name] = FieldFunctionArray(
-                                    (
-                                        need[option_name],
-                                        *field_value.value,
-                                    )
-                                )
-                            else:
-                                raise RuntimeError(
-                                    f"Cannot append non-string/array value {field_value.value!r} to field '{option_name}'"
-                                )
-                    case (ExtendType.REPLACE | ExtendType.DELETE, None):
-                        error_msg = (
-                            "Replace or Delete action is not allowed via needextends."
-                        )
-                        log_warning(logger, error_msg, "needextend", location=location)
-                        raise RuntimeError(f"{location}: {error_msg}")
-                    case (ExtendType.REPLACE | ExtendType.DELETE, FieldLiteralValue()):
-                        error_msg = (
-                            "Replace or Delete action is not allowed via needextends."
-                        )
-                        log_warning(logger, error_msg, "needextend", location=location)
-                        raise RuntimeError(f"{location}: {error_msg}")
-                    case (ExtendType.REPLACE | ExtendType.DELETE, FieldFunctionArray()):
-                        error_msg = (
-                            "Replace or Delete action is not allowed via needextends."
-                        )
-                        log_warning(logger, error_msg, "needextend", location=location)
-                        raise RuntimeError(f"{location}: {error_msg}")
-                        # TODO reset need[option_name] to something sensible?
-                    case other_field:
-                        raise RuntimeError(
-                            f"Unhandled case {other_field} for {option_name!r}"
-                        )
+    return original_function(all_needs, extends, needs_config)
 
 
-sphinx_needs.directives.need.extends_needs_data = score_extend_needs_data_func
-
-print("=====================================")
-print("WE HAVE REPLACED THE EXTENDS FUNC")
-print("=====================================")
+sphinx_needs.directives.need.extend_needs_data = score_extend_needs_data_func
